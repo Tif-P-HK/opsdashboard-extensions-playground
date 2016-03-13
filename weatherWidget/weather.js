f8bfd4b4f30bcd381a7e8e8d312d18ee1e6f88ba
Tif-P-HK <bads_turtleboard@hotmail.com>
2016-03-12 16:20:43 -0800

    - initial commit of weather widget

---
+++ weatherWidget/weather.js
@@ -0,0 +1,161 @@
+define([
+  "dojo/_base/declare",
+  "dojo/_base/lang",
+  "dijit/_WidgetBase",
+  "dijit/_TemplatedMixin",
+  "dojo/dom-class",
+  "esri/opsdashboard/WidgetProxy",
+  "esri/opsdashboard/MapWidgetProxy",
+  "esri/geometry/webMercatorUtils",
+  "esri/config",
+  "esri/request",
+  "dojo/text!./weatherTemplate.html"
+], function (declare, lang, _WidgetBase, _TemplatedMixin, domClass, WidgetProxy, MapWidgetProxy, webMercatorUtils, esriConfig, esriRequest, templateString) {
+
+  return declare("WeatherWidget", [_WidgetBase, _TemplatedMixin, WidgetProxy], {
+    templateString: templateString,
+
+    //TODO:
+    // 1. Handle map widget removal
+    // 2. Add loading page
+
+    // ***************************************************************
+    // NOTE: To run this sample, you must sign up for and enter here a
+    // free Weather Underground Developer API key.
+    // Sign up at http://www.wunderground.com/weather/api/
+    // ****************************************************************
+    hostReady: function () {
+      var domain = "api.wunderground.com";
+      // TODO: remove the following line
+      domain = "gist.githubusercontent.com";
+      esriConfig.defaults.io.corsEnabledServers.push(domain);
+
+      // TODO: replace dev key
+      var _developerKey = "yourDeveloperKey"; // ENTER YOUR WEATHER UNDERGROUND DEVELOPER API KEY HERE
+      this.weatherServiceUrl = "http://" + domain + "/api/" + _developerKey + "/conditions/satellite/webcams/q/";
+
+      // Listen to map extent changed by subscribing to map events
+      //var mapWidget = this.mapWidgetProxy;
+      //mapWidget.subscribeToMapEvents();
+
+      if(!_developerKey){
+        this.showWarning("Enter a Weather Underground developer key to run this sample");
+        return;
+      }
+
+      this.mapWidgetProxy.getMapExtent().then(function(extent){
+
+        // If spatial reference is Web Mercator, convert point to Geographic, otherwise assume Geographic
+        var mapCenter = extent.getCenter();
+        var geoMapCenter = mapCenter.spatialReference.isWebMercator() ?
+          webMercatorUtils.webMercatorToGeographic(mapCenter) : mapCenter;
+
+        // Get the weather information at the map center
+        this.getWeatherInformation(geoMapCenter);
+      }.bind(this), function(error){
+        console.log("Error: ", error.message);
+      });
+    },
+
+    getWeatherInformation: function(geoMapCenter){
+      // Append location information to the weather service URL
+      this.weatherServiceUrl += geoMapCenter.getLatitude() + "," + geoMapCenter.getLongitude() + ".json";
+      // TODO: remove temp url:
+      this.weatherServiceUrl = "https://gist.githubusercontent.com/Tif-P-HK/3dda647389e7250ee0f8/raw/db1ab499bc1a3fafecfb28cf7531b4394e8760ab/weatherUnderground.json";
+      console.log("weather Service Url: " + this.weatherServiceUrl);
+
+      // Request for weather information
+      // TODO: double-check on params
+      esriRequest({
+        url: this.weatherServiceUrl
+      }).then(function(response){
+        console.log("request succeeded, response: " + response);
+        this.displayResult(response);
+      }.bind(this), function(error){
+        this.showWarning("Error getting weather information");
+        console.log("Error: ", error.message);
+      })
+    },
+
+    displayResult: function(response){
+
+      // Store the weather information result (this.observation) and satellite image (this.satellite) separately
+      var observation = response.current_observation;
+      var satellite = response.satellite;
+
+      // Show the location information
+      this.fullLocationName.innerHTML = observation.display_location.full;
+      this.observationTime.innerHTML = observation.observation_time;
+
+      // Display the observation result on details page
+      this.weather.innerHTML = observation.weather;
+
+      this.weatherImg.src = observation.icon_url;
+      this.weatherImgCaption.innerHTML = observation.temp_f + "&deg;F (" + observation.temp_c + "&deg;C); " +
+        "Feels like " + observation.feelslike_f + "&deg;F (" + observation.feelslike_c + "&deg;C)";
+
+      this.uv.innerHTML= observation.UV;
+      this.windInfo.innerHTML = observation.wind_dir + " " + observation.wind_mph + " mph";
+      this.precipitationToday.innerHTML = observation.precip_today_string;
+      this.relativeHumidity.innerHTML = observation.relative_humidity;
+      this.dewPoint.innerHTML = observation.dewpoint_f + "&deg;F (" + observation.dewpoint_c + "&deg;C)";
+      this.visibility.innerHTML = observation.visibility_mi + " mi (" + observation.visibility_km + " km)";
+
+
+      // Display the satellite image result on Map page
+      this.visibleSatelliteImage.src = satellite.image_url_vis;
+    },
+
+    mapExtentChanged: function(){
+      alert("extent changed");
+    },
+
+    showDetailsPage: function(){
+      // Shows the weather details page when the Details button is clicked
+
+      domClass.remove(this.detailsPage, "hide");
+      domClass.add(this.mapPage, "hide");
+      domClass.add(this.warningPage, "hide");
+    },
+
+    showMapPage: function(){
+      // Shows the map page when the Map button is clicked
+
+      domClass.add(this.detailsPage, "hide");
+      domClass.remove(this.mapPage, "hide");
+      domClass.add(this.warningPage, "hide");
+    },
+
+    showWarning: function(message){
+      this.warningMsg.innerHTML = message;
+
+      domClass.add(this.detailsPage, "hide");
+      domClass.add(this.mapPage, "hide");
+      domClass.remove(this.warningPage, "hide");
+      domClass.add(this.navigationDiv, "hide");
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
